0779a8aceaf1526b0eceac1e22d84fde2f5184a1
Vit Benton <Vit@vitbenton.com>
2025-05-02 08:15:07 -0400

    minor lint fixes

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.js';
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 app.use(authRoutes);
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Auth server running at http://localhost:${PORT}`);
 });

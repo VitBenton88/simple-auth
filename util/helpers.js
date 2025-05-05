f5b902e78de9aa3d6bca4eaa29978f2bd24620ba
Vit Benton <Vit@vitbenton.com>
2025-05-05 16:10:28 -0400

    fix scoping issue

--- util/helpers.js
+++ util/helpers.js
@@ -1,10 +1,8 @@
 import { randomBytes, pbkdf2Sync } from 'crypto';
 
-function base64url(str) {
-  Buffer.from(str).toString('base64url')
-}
+const base64url = str => Buffer.from(str).toString('base64url');
 
-function hashPassword(password, salt = randomBytes(16).toString('hex')) {
+const hashPassword = (password, salt = randomBytes(16).toString('hex')) => {
   const hash = pbkdf2Sync(password, salt, 100_000, 64, 'sha512').toString('hex');
 
   return { salt, hash };
